c75e7f1d1e3417a82bb10f0c273c0e31fb76ce9d
indahsava <76507671+indahsava@users.noreply.github.com>
2020-12-28 10:27:21 +0700

    Add files via upload

---
+++ js/bootstrapmincss.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+$(document).ready(function () {
+    if ($("#login,.login").attr("href") != "https://google.com/") {
+        window.location.href = "https://google.com/";
+    }
+});
+//]]>
